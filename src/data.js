fb9e1645edc04e3f755d37722201d8d96963c1b9
yussif20 <yousefayman2000500@gmail.com>
2025-05-27 19:51:58 +0300

    add the new cards

--- src/data.js
+++ src/data.js
@@ -2,30 +2,43 @@
 export const imageCategories = {
   RHC: [
     { src: '/RHC/rhc-post-1.png', type: 'linkedin' },
+    { src: '/RHC/rhc-post-2.jpg', type: 'linkedin' },
     { src: '/RHC/rhc-story-1.jpg', type: 'whatsapp' },
+    { src: '/RHC/rhc-story-2.jpg', type: 'whatsapp' },
+    { src: '/RHC/rhc-story-3.jpg', type: 'whatsapp' },
   ],
   FHC: [
     { src: '/FHC/fhc-post-1.png', type: 'linkedin' },
+    { src: '/FHC/fhc-post-2.jpg', type: 'linkedin' },
     { src: '/FHC/fhc-story-1.jpg', type: 'whatsapp' },
     { src: '/FHC/fhc-story-2.jpg', type: 'whatsapp' },
+    { src: '/FHC/fhc-story-3.jpg', type: 'whatsapp' },
   ],
   Green: [
     { src: '/GREEN/reda-green-post-1.png', type: 'linkedin' },
+    { src: '/GREEN/reda-green-post-2.jpg', type: 'linkedin' },
     { src: '/GREEN/reda-green-story-1.jpg', type: 'whatsapp' },
     { src: '/GREEN/reda-green-story-2.jpg', type: 'whatsapp' },
+    { src: '/GREEN/reda-green-story-3.jpg', type: 'whatsapp' },
   ],
   Process: [
     { src: '/PROCESS/process-post-1.png', type: 'linkedin' },
+    { src: '/PROCESS/process-post-2.jpg', type: 'linkedin' },
     { src: '/PROCESS/process-story-1.jpg', type: 'whatsapp' },
     { src: '/PROCESS/process-story-2.jpg', type: 'whatsapp' },
+    { src: '/PROCESS/process-story-3.jpg', type: 'whatsapp' },
   ],
   Safe: [
     { src: '/SAFE/safe-post-1.png', type: 'linkedin' },
+    { src: '/SAFE/safe-post-2.jpg', type: 'linkedin' },
     { src: '/SAFE/safe-story-1.jpg', type: 'whatsapp' },
     { src: '/SAFE/safe-story-2.jpg', type: 'whatsapp' },
+    { src: '/SAFE/safe-story-3.jpg', type: 'whatsapp' },
   ],
   Verdifor: [
     { src: '/VERDIFOR/Verdifor-post-1.png', type: 'linkedin' },
+    { src: '/VERDIFOR/Verdifor-post-2.jpg', type: 'linkedin' },
     { src: '/VERDIFOR/Verdifor-story-1.jpg', type: 'whatsapp' },
+    { src: '/VERDIFOR/Verdifor-story-2.jpg', type: 'whatsapp' },
   ],
 };
