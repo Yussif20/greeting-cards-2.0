d82c4716165f274fc1101727fa30b83bc1816d7a
yussif20 <yousefayman2000500@gmail.com>
2025-05-28 21:41:52 +0300

    fix the images import states

--- src/data.js
+++ src/data.js
@@ -1,44 +1,87 @@
+//RHC images
+import rhcPost1 from './assets/rhc/rhc-post-1.png'; 
+import rhcPost2 from './assets/rhc/rhc-post-2.jpg';
+import rhcStory1 from './assets/rhc/rhc-story-1.jpg';
+import rhcStory2 from './assets/rhc/rhc-story-2.jpg';
+import rhcStory3 from './assets/rhc/rhc-story-3.jpg';
+
+//FHC images
+import fhcPost1 from './assets/fhc/fhc-post-1.png';
+import fhcPost2 from './assets/fhc/fhc-post-2.jpg';
+import fhcStory1 from './assets/fhc/fhc-story-1.jpg';
+import fhcStory2 from './assets/fhc/fhc-story-2.jpg';
+import fhcStory3 from './assets/fhc/fhc-story-3.jpg';
+
+//Green images
+import greenPost1 from './assets/green/reda-green-post-1.png';
+import greenPost2 from './assets/green/reda-green-post-2.jpg';
+import greenStory1 from './assets/green/reda-green-story-1.jpg';
+import greenStory2 from './assets/green/reda-green-story-2.jpg';
+import greenStory3 from './assets/green/reda-green-story-3.jpg';
+
+//Process images
+import processPost1 from './assets/process/process-post-1.png';
+import processPost2 from './assets/process/process-post-2.jpg'; 
+import processStory1 from './assets/process/process-story-1.jpg';
+import processStory2 from './assets/process/process-story-2.jpg';
+import processStory3 from './assets/process/process-story-3.jpg';
+
+//Safe images
+import safePost1 from './assets/safe/safe-post-1.png';
+import safePost2 from './assets/safe/safe-post-2.jpg';   
+import safeStory1 from './assets/safe/safe-story-1.jpg';
+import safeStory2 from './assets/safe/safe-story-2.jpg';
+import safeStory3 from './assets/safe/safe-story-3.jpg';
+
+
+//Verdifor images
+import verdiforPost1 from './assets/verdifor/verdifor-post-1.png';
+import verdiforPost2 from './assets/verdifor/verdifor-post-2.jpg';
+import verdiforStory1 from './assets/verdifor/verdifor-story-1.jpg';
+import verdiforStory2 from './assets/verdifor/verdifor-story-2.jpg';
+
+
 // src/data.js
 export const imageCategories = {
   RHC: [
-    { src: '/RHC/rhc-post-1.png', type: 'linkedin' },
-    { src: '/RHC/rhc-post-2.jpg', type: 'linkedin' },
-    { src: '/RHC/rhc-story-1.jpg', type: 'whatsapp' },
-    { src: '/RHC/rhc-story-2.jpg', type: 'whatsapp' },
-    { src: '/RHC/rhc-story-3.jpg', type: 'whatsapp' },
+    { src: rhcPost1, type: 'linkedin' },
+    { src: rhcPost2, type: 'linkedin' },
+    { src: rhcStory1, type: 'whatsapp' },
+    { src: rhcStory2, type: 'whatsapp' },
+    { src: rhcStory3, type: 'whatsapp' },
   ],
   FHC: [
-    { src: '/FHC/fhc-post-1.png', type: 'linkedin' },
-    { src: '/FHC/fhc-post-2.jpg', type: 'linkedin' },
-    { src: '/FHC/fhc-story-1.jpg', type: 'whatsapp' },
-    { src: '/FHC/fhc-story-2.jpg', type: 'whatsapp' },
-    { src: '/FHC/fhc-story-3.jpg', type: 'whatsapp' },
+    { src: fhcPost1, type: 'linkedin' },
+    { src: fhcPost2, type: 'linkedin' },
+    { src: fhcStory1, type: 'whatsapp' },
+    { src: fhcStory2, type: 'whatsapp' },
+    { src: fhcStory3, type: 'whatsapp' },
   ],
   Green: [
-    { src: '/GREEN/reda-green-post-1.png', type: 'linkedin' },
-    { src: '/GREEN/reda-green-post-2.jpg', type: 'linkedin' },
-    { src: '/GREEN/reda-green-story-1.jpg', type: 'whatsapp' },
-    { src: '/GREEN/reda-green-story-2.jpg', type: 'whatsapp' },
-    { src: '/GREEN/reda-green-story-3.jpg', type: 'whatsapp' },
+    { src: greenPost1, type: 'linkedin' },
+    { src: greenPost2, type: 'linkedin' },
+    { src: greenStory1, type: 'whatsapp' },
+    { src: greenStory2, type: 'whatsapp' },
+    { src: greenStory3, type: 'whatsapp' },
   ],
   Process: [
-    { src: '/PROCESS/process-post-1.png', type: 'linkedin' },
-    { src: '/PROCESS/process-post-2.jpg', type: 'linkedin' },
-    { src: '/PROCESS/process-story-1.jpg', type: 'whatsapp' },
-    { src: '/PROCESS/process-story-2.jpg', type: 'whatsapp' },
-    { src: '/PROCESS/process-story-3.jpg', type: 'whatsapp' },
+    { src: processPost1, type: 'linkedin' },
+    { src: processPost2, type: 'linkedin' },
+    { src: processStory1, type: 'whatsapp' },
+    { src: processStory2, type: 'whatsapp' },
+    { src: processStory3, type: 'whatsapp' },
   ],
   Safe: [
-    { src: '/SAFE/safe-post-1.png', type: 'linkedin' },
-    { src: '/SAFE/safe-post-2.jpg', type: 'linkedin' },
-    { src: '/SAFE/safe-story-1.jpg', type: 'whatsapp' },
-    { src: '/SAFE/safe-story-2.jpg', type: 'whatsapp' },
-    { src: '/SAFE/safe-story-3.jpg', type: 'whatsapp' },
+    { src: safePost1, type: 'linkedin' },
+    { src: safePost2, type: 'linkedin' },
+    { src: safeStory1, type: 'whatsapp' },
+    { src: safeStory2, type: 'whatsapp' },
+    { src: safeStory3, type: 'whatsapp' },
   ],
   Verdifor: [
-    { src: '/VERDIFOR/verdifor-post-1.png', type: 'linkedin' },
-    { src: '/VERDIFOR/verdifor-post-2.jpg', type: 'linkedin' },
-    { src: '/VERDIFOR/verdifor-story-1.jpg', type: 'whatsapp' },
-    { src: '/VERDIFOR/verdifor-story-2.jpg', type: 'whatsapp' },
+    { src: verdiforPost1, type: 'linkedin' },
+    { src: verdiforPost2, type: 'linkedin' },
+    { src:  verdiforStory1, type: 'whatsapp' },
+    { src: verdiforStory2, type: 'whatsapp' },
   ],
 };
